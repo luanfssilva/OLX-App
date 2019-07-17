b3b6dd9514861a80fa939cf0d1442b46ffe9d4e5
Luan Felipe <luanfelipebtu@hotmail.com>
2019-07-16 23:05:14 -0300

    Criando e configurando projeto

---
+++ app/src/main/java/com/example/luan/olxapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.luan.olxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
