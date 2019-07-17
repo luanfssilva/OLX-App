b2aac19c22baeee4df0024446a68edf22af0e3d2
Luan Felipe <luanfelipebtu@hotmail.com>
2019-07-17 00:06:05 -0300

    Autenticacao do usuario

---
+++ app/src/main/java/com/example/luan/olxapp/activity/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.luan.olxapp.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.luan.olxapp.R;
+import com.example.luan.olxapp.helper.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Switch tipoAcesso;
+    private Button btnAcesso;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        btnAcesso = findViewById(R.id.btnAcessar);
+        tipoAcesso = findViewById(R.id.switchAcesso);
+
+        auth = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+
+        btnAcesso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                if (!email.isEmpty()){
+                    if(!senha.isEmpty()){
+
+                        //Verifica estado do switch
+                        if(tipoAcesso.isChecked()){ //Cadastro
+
+                            auth.createUserWithEmailAndPassword(
+                              email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(task.isSuccessful()){
+                                        showMessage("Cadastro realizado com sucesso!!");
+                                    }
+                                    else {
+                                        String erroExcecao = "";
+
+                                        try{
+                                            throw task.getException();
+
+                                        }catch (FirebaseAuthWeakPasswordException e){
+                                            erroExcecao = "Digite uma senha mais forte!";
+                                        }catch (FirebaseAuthInvalidCredentialsException e){
+                                            erroExcecao = "Por favor, digite um e-mail válido";
+                                        }catch (FirebaseAuthUserCollisionException e){
+                                            erroExcecao = "Este conta já foi cadastrada";
+                                        }catch (Exception e){
+                                            erroExcecao = "ao cadastrar usuário: " + e.getMessage();
+                                            e.printStackTrace();
+                                        }
+
+                                        showMessage(erroExcecao);
+                                    }
+                                }
+                            });
+                        }else{ //Login
+
+                            auth.signInWithEmailAndPassword(
+                                    email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if ( task.isSuccessful()){
+
+                                        showMessage("Logado com sucesso");
+
+                                    }else{
+                                        showMessage("Erro ao fazer login" + task.getException());
+                                    }
+                                }
+                            });
+
+
+                        }
+                    }else{
+                        showMessage("Preencha o campo senha");
+                    }
+                }else {
+                    showMessage("Preencha o campo email");
+                }
+            }
+        });
+
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/luan/olxapp/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,43 @@
+package com.example.luan.olxapp.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference refereciaStorage;
+
+    //retorna a referencia do database
+    public static DatabaseReference getFirebase(){
+        if (referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    //retrona instancia do FirebaseStorage
+    public static StorageReference getFirebaseStorage(){
+        if(refereciaStorage == null){
+            refereciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return refereciaStorage;
+    }
+
+
+
+
+
+}
