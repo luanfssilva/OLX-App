2592c85a6b6ab2ee3edc8eea9f189a830180abdd
Luan Felipe <luanfelipebtu@hotmail.com>
2019-07-18 23:19:48 -0300

    Utilizando mascaras - telefone e moeda

--- app/src/main/java/com/example/luan/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/luan/olxapp/activity/CadastrarAnuncioActivity.java
@@ -3,14 +3,47 @@ package com.example.luan.olxapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.luan.olxapp.R;
+import com.santalu.maskedittext.MaskEditText;
+
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
+    private Button btnCadastrarAnuncio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+        btnCadastrarAnuncio = findViewById(R.id.btnCadastrarAnuncio);
+
+        Locale locale = new Locale("pt","BR");
+        campoValor.setLocale(locale);
+
+        btnCadastrarAnuncio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+
     }
 }
